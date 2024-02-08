c8d9d9a1c6b4b4742272a1e2d84ecc50bb90fad6
Markus Karjalainen <markus.karjalainen@outlook.com>
2024-02-08 16:55:31 +0200

    3.9 puhelinluettelon backend step9. Use cors to allow frontend functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 morgan.token('data',  (req, res) => {
@@ -8,6 +9,7 @@ morgan.token('data',  (req, res) => {
   }
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
